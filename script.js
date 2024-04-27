82139f00ca9a192a3c1eadb49cd81623ec6556ae
February191999 <96108108+February191999@users.noreply.github.com>
2024-04-28 03:54:45 +0700

    Add for loop to create 16 x 16 grid

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 const containerDiv = document.querySelector(".container");
 
+for (let i = 0; i < 256; i++) {
+    const gridDiv = document.createElement("div");
+    gridDiv.style.cssText = "border: 5px solid black; width: 50px; height: 50px;";
+    containerDiv.appendChild(gridDiv);
+}
