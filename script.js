e88a36ba59b035d94623ca8497a55a1f44b921c0
February191999 <96108108+February191999@users.noreply.github.com>
2024-04-29 00:58:07 +0700

    Add while loop to remove child nodes from containerDiv if child nodes present

--- script.js
+++ script.js
@@ -8,36 +8,41 @@ newGridButton.setAttribute("style", "width: 10%; height: 10%; font-weight: bold;
 
 body.insertBefore(newGridButton, containerDiv);
 
-// newGridButton.addEventListener("click", () => {
-//     let askNumber = prompt("Enter a number for your new grid (not more than a 100!): ");
-//     let toInteger = Number(askNumber);
+newGridButton.addEventListener("click", () => {
+    let askNumber = prompt("Enter a number for your new grid (not more than a 100!): ");
+    let toInteger = Number(askNumber);
 
-//     function createNewGrid() {
+    while (containerDiv.hasChildNodes()) {
+        containerDiv.removeChild(containerDiv.firstChild);
+    };
+    
 
-//         for (let i = 1; i < toInteger; i++) { // Create grid according to prompt
-//             const gridDiv = document.createElement("div"); //Individual grids
-        
-//             const dividedHeightParts = (containerHeight / toInteger); //height divided into 16 parts
-//             const gridDivHeightPercentage = ((dividedHeightParts / containerHeight) * 100); //Get percentage each part needs 
-        
-//             gridDiv.setAttribute("style", `border: 5px solid black; width: ${gridDivHeightPercentage}%; height: ${gridDivHeightPercentage}%; margin: 0; padding: 0;`);
-        
-//             console.log(containerDiv);
-        
-//             if (i % 16 == 0) {
-//                 gridDiv.setAttribute("style", `border: 5px solid black; width: ${gridDivHeightPercentage}%; height: ${gridDivHeightPercentage}%; background-color: red; margin: 0; padding: 0;`);
-//             }
-        
-//             gridDiv.addEventListener("mouseover", () => { //Changes grid color if mouse hovers above
-//                 gridDiv.style.backgroundColor = "blue";
-//             });
-        
-//             containerDiv.appendChild(gridDiv);
-//         }
-//     }
+    function createNewGrid() {
 
-//     createNewGrid;
-// })
+        for (let i = 1; i < toInteger; i++) { // Create grid according to prompt
+            const gridDiv = document.createElement("div"); //Individual grids
+        
+            const dividedHeightParts = (containerHeight / toInteger); //height divided into 16 parts
+            const gridDivHeightPercentage = ((dividedHeightParts / containerHeight) * 100); //Get percentage each part needs 
+        
+            gridDiv.setAttribute("style", `border: 5px solid black; width: ${gridDivHeightPercentage}%; height: ${gridDivHeightPercentage}%; margin: 0; padding: 0;`);
+        
+            console.log(containerDiv);
+        
+            if (i % 16 == 0) {
+                gridDiv.setAttribute("style", `border: 5px solid black; width: ${gridDivHeightPercentage}%; height: ${gridDivHeightPercentage}%; background-color: red; margin: 0; padding: 0;`);
+            }
+        
+            gridDiv.addEventListener("mouseover", () => { //Changes grid color if mouse hovers above
+                gridDiv.style.backgroundColor = "blue";
+            });
+        
+            containerDiv.appendChild(gridDiv);
+        }
+    }
+
+    createNewGrid;
+})
 
 for (let i = 1; i < 257; i++) { // Create 16 x 16 grid
     const gridDiv = document.createElement("div"); //Individual grids
